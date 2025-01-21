6761193b805464c24ec8b6ad2cfa750f420c8def
Kevin Nennig <knennig213@gmail.com>
2025-01-21 16:04:05 -0500

    updated for demo

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p137825-e1511252.adobeaemcloud.com';
+  return 'https://publish-p145250-e1496097.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p137825-e1511252.adobeaemcloud.com';
+  return 'https://author-p145250-e1496097.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
